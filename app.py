815f6b40b1008846d4a2565295565b95fceab087
Gajarbotol <123944033+Gajarbotol@users.noreply.github.com>
2024-11-07 17:53:50 +0600

    Update app.py

--- app.py
+++ app.py
@@ -193,4 +193,5 @@ def write_names_and_numbers_to_file(details_list):
             file.write(f"   name - {details['name']}\n\n")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+       port = int(os.environ.get("PORT", 5000))
+       app.run(host='0.0.0.0', port=port)
